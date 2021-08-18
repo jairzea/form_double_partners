1524d1f4245afe11d4c70fa7deb3dd8c694a2322
Jair Zea <46404027+jairzea@users.noreply.github.com>
2021-08-18 17:26:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# form_double_partners
+ Form for creating user account
---
+++ android/app/src/main/kotlin/com/example/form_double_partners/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.form_double_partners
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
